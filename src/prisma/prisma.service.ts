20fce4d1c63fc2378ba03a9a403ead33ab40d3e5
castrintt <igordc38@gmail.com>
2026-03-10 09:05:00 -0300

    feat: Update Prisma schema, regenerate Prisma client, and add dotenv dependency.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,11 +4,6 @@ import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super({
-      accelerateUrl: process.env.DATABASE_URL ?? "",
-    });
-  }
 
   async onModuleInit() {
     await this.$connect();
